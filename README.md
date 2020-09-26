4b33c85fe2adb7936fce221d8a642147bae14d79
millerbee <millerbgos@gmail.com>
2020-09-25 19:04:32 -0600

    updates to Password generator

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Password_Generator
+# This generator prompts the user to verify some parameters to create a password.
+# First, the password must be between 8 and 128 characters long and their entry should be numeric.
+# Second, does the user want to use:
+#            special characters
+#            numbers
+#            upper case letters
+#            lower case letters
+#
+# the JS here verifies the above requirements and stores what the user would like to use.
+# this app also guarantees the user will have a minimum of one each of their choice and also unique, ensuring a repeat value isn't generated.
+# 
+#
+# thank you to my great tutor Tyler Bolty who explains everything so well!
--- script.js
+++ script.js
@@ -6,28 +6,27 @@ var uppers =["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N
 var spChar =["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
 
 
-
+//function to verify user selection
 function getPasswordOptions() {
 
    var pLen = (prompt("How many characters would you like your password?  enter between 8 and 128"));  //ask user how long they want password
            
-    if (pLen < 8 || pLen > 128) {
-            alert("Minimum password length is 8 characters and max is 128 characters");  //alert when user enters less than 8 or greater than 128
+    if (isNaN(pLen) || pLen < 8 || pLen > 128) {
+            alert("Minimum password length is 8 characters and max is 128 characters. Please enter a number.");  //alert when user enters less than 8 or greater than 128
             return
         } 
        
 
-
  var specChar = confirm("Would you like special characters?");
- var upperChar = confirm("Would you like numbers?" );
- var lowerChar =confirm("Would you like uppercase characters?");
- var numberChar =confirm("Would you like lowercase characters?");
+ var numberChar = confirm("Would you like numbers?" );
+ var upperChar =confirm("Would you like uppercase letters?");
+ var lowerChar =confirm("Would you like lowercase letters?");
     
     if (lowerChar === false && upperChar === false && numberChar === false && specChar === false) {
         alert("You must select at least one parameter")                                                 // alert user at least one parameter is required.
-    return                                                                                          //returns value and if no return breaks the function and starts over.
+    return                                                                                              //returns value and if no return breaks the function and starts over.
   }
-  
+// object to be used in generatePassword function
   var passwordOptions = {
       pLen: pLen,
       specChar: specChar,
@@ -38,25 +37,24 @@ function getPasswordOptions() {
   return passwordOptions
 }
 
-function getRandom(arr){       //one random per 
+//  function to randomize the values in the arrays
+function getRandom(arr){       
         
     var randIndex = Math.floor(Math.random() * arr.length);
     return arr[randIndex]
 
 }
-// function to generate Password   -- Ask the user length of password from 8 to 128 chars
+// function to generate Password   
 function generatePassword(){
-   var result=[]                        //random chars of what user asked for
-   var possibleChar=[]                  //all the possible chars of what the user has ok'd
-   var gauranteeChar=[]                 //guarantee a min of 1 of each type, so user doesn't get all numbers etc..
-   var options = getPasswordOptions()    //the one object above
+   var result=[]                                                                                         //random values of what user asked for
+   var possibleChar=[]                                                                                   //all the possible values of what the user has selected
+   var gauranteeChar=[]                                                                                  //guarantee a min of 1 of each type, so a repeating pswd doesn't occur.
+   var options = getPasswordOptions()                                                                     
 
-if (options.specChar === true) {
-    possibleChar=possibleChar.concat(spChar)
+if (options.specChar === true) {                                                                         //these if conditions take the values from generatePassword function,
+    possibleChar=possibleChar.concat(spChar)                                                             //concatentates and tells getRandom function to perform the randomizing.
     gauranteeChar.push(getRandom(spChar))
-    
-}
- // do this 3x for the others.   
+     
  if (options.upperChar === true) {
     possibleChar=possibleChar.concat(uppers)
     gauranteeChar.push(getRandom(uppers))
@@ -73,14 +71,14 @@ if (options.numberChar === true) {
     
 }
 
- for(var i=0; i < options.pLen; i++) {
+ for(var i=0; i < options.pLen; i++) {                                                                  //this for loop will take values and increment creating a result array.        
      result.push(getRandom(possibleChar))
  }
 
  for(var i=0; i < gauranteeChar.length; i++) {
     result[i]=gauranteeChar[i]
 }
-  return result.join("")                            //joining the result array to a string.
+  return result.join("")                                                                                //joining the result array to a string.
 }   
 
            
